4caddc97779c7d947ebe8e01db9fb553f29f87b3
Emerson89 <emerson.cesario50@gmail.com>
2023-04-15 15:47:29 -0300

    pre-commit

--- examples/locals.tf
+++ examples/locals.tf
@@ -1,5 +1,5 @@
 locals {
-   ingress_ec2 = {
+  ingress_ec2 = {
     "ingress_rule_1" = {
       "from_port"   = "0"
       "to_port"     = "65535" ##all internal traffic 
@@ -7,5 +7,5 @@ locals {
       "cidr_blocks" = ["10.10.0.0/16"]
     },
   }
-  
+
 }
--- examples/providers.tf
+++ examples/providers.tf
@@ -1,15 +1,15 @@
 provider "aws" {
-  region                      = "us-east-1"
-  profile                     = "local"
+  region  = "us-east-1"
+  profile = "local"
 
   ## necessário para uso com localstack
-  skip_credentials_validation = true 
+  skip_credentials_validation = true
   skip_metadata_api_check     = true
   skip_requesting_account_id  = true
 
   endpoints {
-    iam        = "http://localhost:4566"
-    ec2        = "http://localhost:4566"
+    iam = "http://localhost:4566"
+    ec2 = "http://localhost:4566"
 
   }
 }
